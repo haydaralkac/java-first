2dae276f318311b6914de56365fc61c2ac23abaf
HydR <alkachaydar@gmail.com>
2021-10-02 15:54:57 +0300

    dosyalar geliyor

---
+++ Ders-3/app.js
@@ -0,0 +1,28 @@
+var a =10;
+var b =2;
+var c =3;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a%c);
+// console.log(a**c);
+// console.log(a**c);
+
+// a = --b
+// a = b--
+// console.log({a})
+// console.log({b})
+
+// var c = 100+50*3
+// console.log(c)
+
+// // a = ++b
+// a = b++
+
+// console.log({b})
+// console.log({a})
+
+var d = a / b  
+console.log(typeof d, d)
